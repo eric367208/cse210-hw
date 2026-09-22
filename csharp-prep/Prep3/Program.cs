cf5bc9d0ac4d6c1b4fa1807072a9a88015097f39
eric367208 <ericames367@gmail.com>
2026-09-22 17:16:16 -0600

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,24 @@
-using System;
+Random randomGenerator = new Random(); //Generates a random number.
 
-class Program
+int Number = randomGenerator.Next(1,100); //Sets range of random number and sets it as an int.
+
+int guess = 0; //initializes the guess
+
+Console.WriteLine("Guess the magic number. "); //Prompts the user to guess the number.
+
+while (guess != Number) //Sets a loop until the correct number is guessed.
 {
-    static void Main(string[] args)
+
+    guess = int.Parse(Console.ReadLine()); //Converts the guess to an int.
+
+    if (guess < Number) //If statement to give direction to the user to make a better guess.
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Higher, guess again. ");
+    }
+    else if (guess > Number)
+    {
+        Console.Write("Lower, guess again. ");
     }
 }
+
+Console.WriteLine("You guessed the magic number! "); //congratulates the user upon a successful guess.
